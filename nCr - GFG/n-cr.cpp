38b7d46671a4c754d96669e7c076c96b4d91a681
Yash Sancheti <32770175+Onyx2406@users.noreply.github.com>
2022-05-19 09:55:57 -0400

    Added solution - LeetHub

--- nCr - GFG/n-cr.cpp
+++ nCr - GFG/n-cr.cpp
@@ -14,15 +14,12 @@ public:
         int C[r+1];
         memset(C, 0, sizeof(C));
         
-        C[0] = 1; // Top row of Pascal Triangle 
+        C[0] = 1;
 
-    	// One by constructs remaining rows of Pascal 
-    	// Triangle from top to bottom 
     	for (int i = 1; i <= n; i++) { 
-    		// Fill entries of current row using previous 
-    		// row values 
+    		
     		for (int j = min(i, r); j > 0; j--){ 
-                // nCj = (n-1)Cj + (n-1)C(j-1); 
+
     			C[j] = (C[j] + C[j-1])%mod;
     		}
     	} 
