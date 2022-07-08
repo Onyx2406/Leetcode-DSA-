0da8c7fc6dcf7e597448fa907d0f3cdaf60f51fd
Yash Sancheti <32770175+Onyx2406@users.noreply.github.com>
2022-07-08 14:50:53 +0530

    Time: 3 ms (91.33%), Space: 9.6 MB (5.24%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,33 +1,23 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m  = matrix.size();
-        if(m == 0)  return false;
-        
-        int n = matrix[0].size();
-        
-        int left=0,right=m*n-1;
-        
-        int pivotidx,pivotele;
-        while(left<=right)
-        {
-            pivotidx=(left+right)/2;
-            pivotele=matrix[pivotidx/n][pivotidx%n];
-            if(target==pivotele)
-            {
+        int row = matrix.size();
+        if(row == 0) return false;
+        int col = matrix[0].size();
+
+        int row_bottom = row -1;
+        int col_left = 0;
+        while(row_bottom >=0 && col_left<col){
+            if(matrix[row_bottom][col_left] == target)
                 return true;
+            else if(matrix[row_bottom][col_left] > target)
+                row_bottom--;
+            else {
+                col_left++;
+                if(col_left <= col-1 && matrix[row_bottom][col_left] > target)
+                    break;
             }
-            else
-            {
-                if(target<pivotele)
-                {
-                    right=pivotidx-1;
-                }
-                else
-                {
-                    left=pivotidx+1;
-                }
-            }
+
         }
         return false;
     }
