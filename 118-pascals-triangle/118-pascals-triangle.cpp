1ebe3c99a31385dc9fcff86e36f97934d4930b2d
Yash Sancheti <32770175+Onyx2406@users.noreply.github.com>
2022-07-09 19:49:17 +0530

    Time: 3 ms (38.74%), Space: 6.5 MB (32.16%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> tri(numRows);
-        
-        for(int i=0;i<numRows;i++)
-        {
-            tri[i].resize(i+1); //if you dont want to do this we can avoid by using pushback 
-            tri[i][0]=1;
-            tri[i][i]=1;
-            
-            for(int j=1;j<i;j++)
-            {
-                tri[i][j]=tri[i-1][j] +tri[i-1][j-1];
-            }
-            
-        }
-        return tri;
-    }
+	vector<vector<int>> ret;
+	for (int i = 0; i < numRows; i++) {
+		vector<int> row(i + 1, 1);
+		for (int j = 1; j < i; j++) {
+			row[j] = ret[i - 1][j] + ret[i - 1][j - 1];
+		}
+		ret.push_back(row);
+	}
+	return ret;
+}
 };
