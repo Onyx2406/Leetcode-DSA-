ceaa9aec94b6cc4890babe675c70e7ecd946ed9c
Yash Sancheti <32770175+Onyx2406@users.noreply.github.com>
2022-07-11 19:59:24 +0530

    Time: 142 ms (46.76%), Space: 14.4 MB (58.87%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,37 +1,16 @@
 class Solution {
 public:
-int coinChange(vector<int> coins, int amount) {
-int n=coins.size();
-
-    int dp[n+1][amount+1];
-    
-    for(int i=0;i<n+1;i++)
-        dp[i][0]=0;  
-        
-    
-    for(int i=1;i<amount+1;i++)
-        dp[0][i]=INT_MAX-1;
-    
-    for(int i=1;i<amount+1;i++)
-    {
-        if(i%coins[0]==0)
-             dp[1][i]=i/coins[0];
-        else
-             dp[1][i]=INT_MAX-1;
-        
-    }
-    
-    for(int i=2;i<n+1;i++)
-    {
-        for(int j=1;j<amount+1;j++)
-        {
-            if(coins[i-1]<=j)
-                dp[i][j]=min(1+dp[i][j-coins[i-1]], dp[i-1][j]);
-            else
-                dp[i][j]=dp[i-1][j];
+    int coinChange(vector<int>& coins, int amount) {
+        int Max = amount + 1;
+        vector<int> dp(amount + 1, Max);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.size(); j++) {
+                if (coins[j] <= i) {
+                    dp[i] = min(dp[i], dp[i - coins[j]] + 1);
+                }
+            }
         }
+        return dp[amount] > amount ? -1 : dp[amount];
     }
-
-    return dp[n][amount]==INT_MAX-1?-1:dp[n][amount];
-}
 };
