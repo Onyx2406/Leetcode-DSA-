ee0bc849c229dcbcfa26ad7c175db00f7d82bd22
Yash Sancheti <32770175+Onyx2406@users.noreply.github.com>
2022-07-13 17:29:07 +0530

    Time: 13 ms (90.82%), Space: 10.7 MB (65.69%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,38 +1,33 @@
 class Solution {
 public:
-    int longestCommonSubsequence(string s, string t) {
-    // tabulation
-    int m = s.size();
-    int n = t.size();
-    if(m == 0 || n == 0) {
-        return 0;
-    }
-    int** dp = new int*[m + 1];
-    for(int i = 0; i <= m; i++) {
-        dp[i] = new int[n + 1];
-    }
-    dp[0][0] = 0;
-    // filling first row with all 0s
-    for(int j = 1; j <= n; j++) {
-        dp[0][j] = 0;
-    }
-    // filling first col with all 0s
-    for(int i = 1; i <= m; i++) {
-        dp[i][0] = 0;
-    }
-
-    for(int i = 1; i <= m; i++) {
-        for(int j = 1; j <= n; j++) {
-            // most imp part
-            if(s[m - i] == t[n - j]) {
-                dp[i][j] = 1 + dp[i - 1][j - 1];
-            }
-            else {
-                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+    int longestCommonSubsequence(string x, string y) {
+        int n=x.size(),m=y.size();
+        string s;
+        int dp[n+1][m+1];
+        for(int i=0;i<=n;i++)dp[i][0]=0;
+        for(int i=0;i<=m;i++)dp[0][i]=0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(x[i-1]==y[j-1])dp[i][j]=1+dp[i-1][j-1];
+                else {
+                    dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
+                }
             }
         }
-    }
-    return dp[m][n];
-        
+        // for printing longest common subsequence
+        // int i=n,j=m;
+        // while(i>0 && j>0){    // if any of this i and j becomes 0 means one string is empty 
+        //     if(x[i-1]==y[j-1]){
+        //         s+=x[i-1];    //if character matches then add that to result string and move diagonally up
+        //         i--;j--;
+        //     }
+        //     else{
+        //         if(dp[i][j-1]>dp[i-1][j])j--;    // else find the max of up and right position of dp table and move accordingly to up or left
+        //         else i--;
+        //     }
+        // }
+        // reverse (s.begin(),s.end());    // reverse the string as we started from last
+        // cout<<s;
+        return dp[n][m];
     }
 };
