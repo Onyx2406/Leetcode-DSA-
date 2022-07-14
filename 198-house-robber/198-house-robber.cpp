a7d57741bf8f0395b789a5880b88c10e48d9f3fe
Yash Sancheti <32770175+Onyx2406@users.noreply.github.com>
2022-07-15 01:15:57 +0530

    Time: 3 ms (54.07%), Space: 7.9 MB (24.74%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
-    int rob(vector<int>& nums) {
-        
-        vector dp (nums.size(), 0);
-        
-        for(int i=0;i<nums.size();i++)
-        {
-            if(i == 0)  {dp[i] = nums[i];}
-            else if(i == 1) {dp[i] = max(dp[i-1],nums[i]);}
-            else    {
-                dp[i] = max(nums[i] + dp[i-2] , dp[i-1]);
-            }
+    int solve(int i, vector<int> &dp,vector<int> &nums){
+	//if size is 0 it means no house to rob so profit=0
+        if(i==0)
+            return 0;
+	//if size is 1 it means single house to rob
+        if(i==1){
+            return nums[0];
         }
-        return dp[nums.size()-1];
-//         int sum1=0,sum2=0;
-        
-//         for(int i=0;i<nums.size();i+=2)
-//         {
-//             sum1+=nums[i];
-//         }
-//         for(int i=1;i<nums.size();i+=2)
-//         {
-//             sum2+=nums[i];
-//         }
-//         return max(sum1,sum2);
-        
+	//if value is already calculated due to overlapping subproblem then return
+        if(dp[i]!=-1)
+            return dp[i];
+	//There are only two cases: house robbed and house not robbed
+	
+	//if a house is robbed then adjancent house cannot be robbed so add the value of ith house robbed ie i-1 index and call recursion after skipping adjacent house
+        int pick = solve(i-2,dp,nums)+nums[i-1];
+	//if a house is not robbed then nothing to add in profit, call recursion in the rest of the house. no skipping required as the house is not robbed
+        int notpick =solve(i-1,dp,nums);
+
+	//choose the one with max profit
+        return dp[i]= max(pick,notpick);
+    }
+    int rob(vector<int>& nums) {    
+        int n=nums.size();
+        vector<int> dp(n+1,-1);
+        return solve(n,dp,nums);       
     }
 };
