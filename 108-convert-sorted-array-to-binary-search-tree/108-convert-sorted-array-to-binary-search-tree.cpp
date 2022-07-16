f785c5c5f2f8ab335f7cefcc84e0979952d9cee1
Yash Sancheti <32770175+Onyx2406@users.noreply.github.com>
2022-07-17 01:05:41 +0530

    Time: 17 ms (75.57%), Space: 21.3 MB (76.70%) - LeetHub

--- 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.cpp
+++ 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.cpp
@@ -1,34 +1,24 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
-public:
-    TreeNode* sortedArrayToBST(vector<int>& nums)
-    {
-        int left = 0;
-        int right = nums.size()-1;
-        return createBST(nums,left,right);
-    }
-    
-    TreeNode* createBST(vector<int> nums,int left,int right)
-    {
-        TreeNode* newroot;
-        if(left <= right)
-        {
-            int mid = (left+right) / 2;
-            newroot = new TreeNode(nums[mid]);
-            newroot->left = createBST(nums,left,mid-1);
-            newroot->right = createBST(nums,mid+1,right);
-            return newroot;
+private:
+    TreeNode* solve(vector<int> &nums,int s,int e){
+        if(s > e){
+            return NULL;
         }
-        return NULL;
+        
+        int mid = s + (e - s) / 2;
+        TreeNode* root = new TreeNode(nums[mid]);
+        // Recursively construct tree in left and right sub part
+        
+        root->left = solve(nums,s,mid-1);
+        root->right = solve(nums,mid+1,e);
+        return root;           
+    }
+
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        int n = nums.size();
+        TreeNode* root = solve(nums,0,n-1);
+        
+        return root;
     }
 };
