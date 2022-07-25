7ed09a6a5fac8ef06f1433e05836a04fbd946ff1
Yash Sancheti <32770175+Onyx2406@users.noreply.github.com>
2022-07-26 00:19:32 +0530

    Time: 205 ms (6.01%), Space: 35.2 MB (93.13%) - LeetHub

--- 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
+++ 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
-    int minDifference(vector<int>& a) {
-        sort(begin(a),end(a));
-        int n=a.size();
-        if(n<=3)
-            return 0;
-        int i=3,j=n-1,r=INT_MAX;
-        while(i>=0)
-            r=min(r,a[j--]-a[i--]);
-        return r;
+    int minDifference(vector<int>& nums) {
+        sort(nums.begin(), nums.end()); int n= nums.size();
+        if(n==1 || n==2 || n==3 || n==4){ return 0; }
+        
+        int a= nums[n-4]-nums[0]; // kill 3 biggest elements
+        int b= nums[n-3]-nums[1]; // kill 2 biggest elements + 1 smallest elements
+        int c= nums[n-2]-nums[2]; // kill 1 biggest elements + 2 smallest elements
+        int d= nums[n-1]-nums[3]; // kill 3 smallest elements
+        
+        return min(a, min(b, min(c,d)));
     }
 };
