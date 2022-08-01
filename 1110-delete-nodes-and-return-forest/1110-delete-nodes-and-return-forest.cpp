f9f39214fa8e6a5a06fe9ba331d3fdf5925e2734
Yash Sancheti <32770175+Onyx2406@users.noreply.github.com>
2022-08-01 13:40:08 +0530

    Time: 1818 ms (5.00%), Space: 419 MB (5.00%) - LeetHub

--- 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.cpp
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    vector<TreeNode*>ans;
-    set<int>s;
-    void solve(TreeNode* &root){
-     if(root!=NULL){
-            solve(root->left);
-            solve(root->right);
-            if(s.find(root->val)!=s.end()){
-                if(root->left)ans.push_back(root->left);
-               if(root->right) ans.push_back(root->right);
-                root=NULL;
-                delete root;
-            }
+    void postorder(TreeNode* &root, map<int,bool> mp, vector<TreeNode*>& res){
+        
+        if(root==NULL) return;
+        postorder(root->left,mp,res);
+        postorder(root->right,mp,res);
+        
+        if(mp[root->val]){
+            if(root->left) res.push_back(root->left);
+            if(root->right) res.push_back(root->right);
+            root=NULL;
         }
     }
-    vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
-       for(int i=0;i<to_delete.size();i++){
-           s.insert(to_delete[i]);
-       }
-        
-        solve(root);
-        if(root){
-            ans.push_back(root);
+    
+    vector<TreeNode*> delNodes(TreeNode* root, vector <int>& to_delete) {
+        vector<TreeNode*> ans;
+        map<int,bool> mp;
+        int n=to_delete.size();
+        for(int i=0;i<n;i++){
+            mp[to_delete[i]]=true;
         }
-        return ans;
+        vector<TreeNode*> res;
+        postorder(root,mp,res);
+        if(root!=NULL) res.push_back(root);
+        return res;
     }
 };
