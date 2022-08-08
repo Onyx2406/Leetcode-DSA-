91f5ef7c3afbd039b3cff7d80aae9f54d69d9107
Yash Sancheti <32770175+Onyx2406@users.noreply.github.com>
2022-08-08 20:12:45 +0530

    Time: 31 ms (23.09%), Space: 8.1 MB (37.92%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -1,41 +1,39 @@
 class Solution {
 public:
-    int value( char r)
-    {
-        if(r=='I') return 1;
-        if(r=='V') return 5;
-        if(r=='X') return 10;
-        if(r== 'L') return 50;
-        if(r== 'C') return 100;
-        if(r=='D') return 500;
-        if(r=='M') return 1000;
-        
-    return -1;
-    }
     int romanToInt(string s) {
-        int res=0;
-        for(int i=0;i<s.length();i++)
+        // creating a map to store the character and its value
+        unordered_map<char,int> m;
+        m['I']=1;
+        m['V']=5;
+        m['X']=10;
+        m['L']=50;
+        m['C']=100;
+        m['D']=500;
+        m['M']=1000;
+        int n=s.length();
+        int ans=0;
+        for(int i=0;i<n;)
         {
-            int s1=value(s[i]);
-            if(i+1 <s.length())
+            // if the value of the next element is greater than previous one 
+            // for example if we encounter IX , then basically we need to subtract the value of 'I' from value of 'X' and then add to answer and then also increment the iterator i by 2 , as we have already considered i+1 element
+            // 'IX'= 10-1=9
+            // 'XL'= 50-10=40
+            // 'IV'= 5-1=4
+            if(i+1<n && m[s[i]]<m[s[i+1]])
             {
-                int s2= value(s[i+1]);
-                if(s1 >= s2)
-                {
-                    res= res +s1;
-                }
-                else
-                {
-                    res=res+s2-s1;
-                    i++;
-                }
+                ans=ans+m[s[i+1]]-m[s[i]];
+                i=i+2;
             }
+            // else we can simply add the value of s[i] to answer and increment i by 1
+            // example- 'VIII' = 5+1+1+1 = 8
+            //          'LX' = 50+10=60    
             else
             {
-                res=res+s1;
+                ans=ans+m[s[i]];
+                i++;
             }
         }
-    return res;
+        return ans;
         
     }
 };
