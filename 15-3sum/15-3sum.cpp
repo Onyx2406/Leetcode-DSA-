b0fbadd5d3154a0acfe976eaa1c1f9d41fdb1933
Yash Sancheti <32770175+Onyx2406@users.noreply.github.com>
2022-08-15 16:52:00 +0530

    Time: 151 ms (51.04%), Space: 21.1 MB (44.46%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,37 +1,37 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> res;
-sort(nums.begin(),nums.end());
-for(int i=0;i<nums.size()-2;i++)
-{
-if(i==0 || (i>0 && nums[i]!=nums[i-1]))
-{
-int l=i+1,h=nums.size()-1,sum =0 - nums[i];
+    vector<vector<int>> res;
+    sort(nums.begin(),nums.end());
+    for(int i=0;i<nums.size()-2;i++)
+    {
+    if(i==0 || (i>0 && nums[i]!=nums[i-1]))
+    {
+    int l=i+1,h=nums.size()-1,sum =0 - nums[i];
 
-            while(l<h)
-            {
-                if(sum == nums[l]+nums[h])
+                while(l<h)
                 {
-                    vector<int> temp(3);
-                    temp[0]=nums[i];
-                    temp[1]=nums[l];
-                    temp[2]=nums[h];
-                    res.push_back(temp);
-                    while(l<h && nums[l]==nums[l+1]) 
+                    if(sum == nums[l]+nums[h])
+                    {
+                        vector<int> temp(3);
+                        temp[0]=nums[i];
+                        temp[1]=nums[l];
+                        temp[2]=nums[h];
+                        res.push_back(temp);
+                        while(l<h && nums[l]==nums[l+1]) 
+                            l++;
+                        while(l<h && nums[h]==nums[h-1])
+                            h--;
                         l++;
-                    while(l<h && nums[h]==nums[h-1])
                         h--;
-                    l++;
-                    h--;
+                    }
+                    else if(nums[l]+nums[h] <sum)
+                        l++;
+                    else
+                        h--;
                 }
-                else if(nums[l]+nums[h] <sum)
-                    l++;
-                else
-                    h--;
             }
         }
-    }
-    return res;
+        return res;
     }
 };
