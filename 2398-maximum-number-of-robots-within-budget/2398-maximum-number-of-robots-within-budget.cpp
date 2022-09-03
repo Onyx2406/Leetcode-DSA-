66c22fd9df09d164b15a111bf6906c4b1432829d
Yash Sancheti <32770175+Onyx2406@users.noreply.github.com>
2022-09-03 22:17:09 +0530

    Time: 1038 ms (20.00%), Space: 167.1 MB (20.00%) - LeetHub

--- 2398-maximum-number-of-robots-within-budget/2398-maximum-number-of-robots-within-budget.cpp
+++ 2398-maximum-number-of-robots-within-budget/2398-maximum-number-of-robots-within-budget.cpp
@@ -1,53 +1,78 @@
 class Solution {
 public:
-    int maximumRobots(vector<int>& chargeTimes, vector<int>& runningCosts, long long budget) {
-        int ans=0;
-        int n=chargeTimes.size();
-
-        vector <long long> prefix(n,0);
-        prefix[0]=runningCosts[0];
-        for(int i=1;i<n;i++){
-            prefix[i]+=prefix[i-1]+runningCosts[i];
+    int maximumRobots(vector<int>& charge, vector<int>& run, long long budget) {
+        int ans = 0;
+        
+        //we will use two pointer 
+        //we will find the cost and also the 
+        //let us have prefix array of sum of cost of subarray in range in O(1)
+        
+        int n = run.size();
+        vector<long long>pref(n);
+        long long tot = 0;
+        
+        for(int i=0;i<n;++i){
+            tot+=run[i];
+            pref[i] = tot;
         }
         
-        long long cost=0;
-        long long p=0,q=0;
-        priority_queue <pair<long long,int>> pq;
+        set<int>P;
+        map<int,int>count;
+        //so we will use set to find the max element in O(log n)
+        //we will use count to remove the elements
         
-        while(q<n){
-            while(q<n){
-                pq.push({chargeTimes[q],q});
-                if(p==0){
-                    cost=prefix[q]*(q-p+1)+pq.top().first;
-                }
-                else{
-                    cost=(prefix[q]-prefix[p-1])*(q-p+1)+pq.top().first;
-                }
-                
-                if(cost<=budget){
-                    if(ans<q-p+1){
-                     ans=q-p+1;
-                   }
-                }
-                else 
-                    break;
-                q++;
-            }
+        tot = 0;
+        int c = 0,start = 0;
+        long long z;
+        for(int i=0;i<n;++i){
+           //I choose the current value 
+            //add in set and increment frequency of it
+            P.insert(charge[i]);
+            count[charge[i]]++;
+            c++;
             
-            if(q>=n){
-                break;
-            }
+            //now find the current score
+            tot = pref[i];
+            if(start)tot-=pref[start-1];
+            z = c;
+            tot*=z;
+            auto it = P.rbegin();
+            long long val = *it;
             
-            if(p<q){
-                p++;
+            tot += val;
+           
+            
+            //now if this score is within the budget then we store the max size of subarray in answer
+            if(tot<=budget){
+                ans = max(ans,c);
             }
             else{
-                p++;
-                q++;
-            }
-            
-            while(!pq.empty() && pq.top().second<p){
-                pq.pop();
+                //now I need to move my left poitner until
+                //the score is less than budget
+                
+                while(start<=i){
+                    
+                    if(count[charge[start]] == 1){
+                        P.erase(charge[start]);
+                    }
+                    count[charge[start]]--;
+                    start++;
+                    c--;
+                    
+                    //this means our subarray is empty
+                    if(start>i)break;
+                    
+                    auto it2 = P.rbegin();
+                    val = *it2;
+                    long long cur_score = 0;
+                    cur_score += pref[i];
+                    cur_score -= pref[start-1];
+                    z = c;
+                    cur_score*=c;
+                    cur_score+=val;
+                    
+                    if(cur_score<=budget)break;
+                }
             }
         }
         
